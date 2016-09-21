8b17196bddeaf6eee477e87839637c6c2cd95772
Max Bo <max@maxbo.me>
2016-09-21 17:28:19 +1000

    Create the pipes

--- assign3/main.c
+++ assign3/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define READ_DESCRIPTOR 0
+#define WRITE_DESCRIPTOR 1
+
 char *get_error_message(int errno) {
 	switch (errno) {
 		case 1:
@@ -36,15 +39,22 @@ char *get_error_message(int errno) {
  */
 int main(int argc, char **argv) {
 
-	// fork
+	int toChildPipe[2];
+    int toParentPipe[2];
+
+    pipe(toChildPipe);
+    pipe(toParentPipe);
 
 	if (fork() == 0) {
-
-
+		close(toChildPipe[WRITE_DESCRIPTOR]);
+		close(toParentPipe[READ_DESCRIPTOR]);
 		// dup2
 		printf("%s\n", "Hello from child");
  		execl("/usr/bin/ls", "ls", "-al", 0);
  	} else {
+ 		close(toChildPipe[READ_DESCRIPTOR]);
+ 		close(toParentPipe[WRITE_DESCRIPTOR]);
+
 	 	wait(NULL);
 	 	printf("%s\n", "Hello from parent");
  	}
