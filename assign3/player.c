17709af3b727e10e2f5f7b492222a9ef24a119a1
Max Bo <max@maxbo.me>
2016-09-22 17:43:06 +1000

    Add more player validation

--- assign3/player.c
+++ assign3/player.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "utils.c"
 
@@ -35,4 +36,32 @@ int main(int argc, char **argv) {
 	fflush(stdout);
 
     return 0;
+}
+
+void validates_args(int argc, char **argv) {
+	int status;
+
+	if (argc != 3) {
+		status = 1;
+	} else if (2 <= atoi(argv[1]) && atoi(argv[1]) <= 26) {
+		status = 2;
+	} else if (strlen(argv[2]) == 1 &&
+		'A' <= argv[2][0] && argv[2][0] <= 'Z') {
+		status = 3;
+	} else {
+		return;
+	}
+
+	fprintf(stderr, "%s\n", get_error_message(status));
+	exit(status);
+}
+
+void validate_args_no(int argc, char **argv) {
+}
+
+void validate_players(int argc, char **argv) {
+}
+
+void validate_ID(int argc, char **argv) {
+
 }
