ab09747c7a56b1174f81ecb07baff591b18b7746
Max Bo <max@maxbo.me>
2016-09-27 01:30:48 +1000

    Pseudofunctional EAIT strategy

--- assign3/state.c
+++ assign3/state.c
@@ -2,6 +2,11 @@
 
 #include "faculty.c"
 
+#define DICE 6
+#define REROLL_LENGTH 14
+#define REROLL_DICE_OFFSET 7 
+#define PROCESS_LENGTH 5
+
 typedef struct State {
     Faculty **faculties;
     char me;
@@ -22,17 +27,61 @@ State *new_state(int numberOfPlayers, char me) {
 }
 
 char *process_roll(State *self, char *rolls) {
-    char *response = malloc(sizeof(char) * 16);
+    char *response = malloc(sizeof(char) * REROLL_LENGTH);
+    memset(response, '\0', REROLL_LENGTH);
+    strcpy(response, "reroll ");
 
-    strcpy(response, "reroll 123456");
+    bool toKeep[6] = { false, false, false, false, false, false };
+
+    // 1 in the 1th column, 2 in the 2th, etc.
+    int tallys[3] = { 0, 0, 0 };
+
+    // If they have more than 2 of the same number (1, 2 or 3), 
+    // they will keep them
+
+    // First, tally up the numbers
+    for (int i = 0; i < DICE; i++) {
+        if (rolls[i] == '1' || rolls[i] == '2' || rolls[i] == '3') {
+
+            // Treat '1' as a 0 for indexing
+            int index = rolls[i] - '1';
+            tallys[index]++;
+        }
+    }
+
+    // Then set the flags
+    for (int i = 0; i < DICE; i++) {
+        if (rolls[i] == '1' || rolls[i] == '2' || rolls[i] == '3') {
+            int index = rolls[i] - '1';
+            if (tallys[index] > 2) {
+                toKeep[i] = true;
+            }
+            // TODO: Health
+        } else if (rolls[i] == 'H') {
+            toKeep[i] = true;
+        }
+    }
+
+    // Then build the string
+    for (int i = 0; i < DICE; i++) {
+        if (!toKeep[i]) {
+            char dieNo = i + '1';
+            response[REROLL_DICE_OFFSET + i] = dieNo;
+        }
+    }
+
 
     return response;
 }
 
 char *process_stay(State *self) {
-    char *response = malloc(sizeof(char) * 5);
+    char *response = malloc(sizeof(char) * PROCESS_LENGTH);
 
     strcpy(response, "stay");
 
     return response;
+}
+
+int label_to_index(char me) {
+    return me - 'A';
 }
